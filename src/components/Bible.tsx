cbf79b01799f148807d393cd3c1bf7785902a095
Renegildo <renegildo@proton.me>
2024-02-26 14:02:47 -0300

    better responsivity for larger screens and increased chapter font

--- src/components/Bible.tsx
+++ src/components/Bible.tsx
@@ -12,8 +12,8 @@ const Bible = ({ currentBook }: BibleProps) => {
 					(book.abbrev === currentBook) && <div>
 						<h1 className='font-black text-4xl serif text-center bg-[#22333B] py-3 mb-10 tracking-wider shadow-lg' key={book.name} id={book.name}>{book.name}</h1>
 						{book.chapters.map((chapters, chapterIndex) => (
-							<div className='mx-5 mb-10'>
-								<h2 className='text-xl my-3 font-semibold serif'>Capítulo {chapterIndex + 1}:</h2>
+							<div className='mx-5 mb-10 max-w-[800px] min-[800px]:mx-auto'>
+								<h2 className='text-3xl my-3 font-bold serif'>Capítulo {chapterIndex + 1}:</h2>
 								{chapters.map((chapter, verseIndex) => (<p className='inline'>
 									<span className='text-[#2EC4B6] text-xs mx-1 font-bold'>{verseIndex + 1}</span>
 									{chapter}</p>))}
